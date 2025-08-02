5aa18b3c8f9e4c3e2eecadb8df3c11c9755a7ed3
pavelgusev13041-code <pavel.gusev13041@gmail.com>
2025-08-02 15:41:38 +0500

    test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,17 @@
-#!bin/bash
+p#!bin/bash
 payza=60
 #провека запуска процесса
 RABOTA=0
 if pgrep -x test; then
     RABOTA=1
 fi
+#запись в лог файл 
 while true; do
 if [ $RABOTA -eq 1 ]; then
     echo "[($data +'%H:%M:%S')] процесс запущен" >> /var/log/monitoring.log
-curl https://test.com/monitoring/test/api
+    curl -Xk https://test.com/monitoring/test/api
 else
     echo "[($data +'%H:%M:%S')] процесс остановлен" >>  /var/log/monitoring.log
-   break
 sleep=$payza
 fi
 done
